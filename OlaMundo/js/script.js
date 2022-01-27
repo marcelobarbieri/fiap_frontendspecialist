4f58d6422de454d60bb806f3c742913d03fc3c08
marcelobarbieri <marcelodpbarbieri@gmail.com>
2022-01-27 19:51:26 -0300

    first commit

---
+++ OlaMundo/js/script.js
@@ -0,0 +1,3 @@
+let nome = prompt("Digite o seu nome: ")
+//alert(`Olá ${nome}, seja bem-vindo a imersão de Front End`)
+document.getElementById("saida").innerHTML = `Olá ${nome}, seja bem-vindo a imersão de Front End`
